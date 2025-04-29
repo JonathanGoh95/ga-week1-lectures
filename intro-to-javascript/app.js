985b5f77d13eab73a8b83984a4f446543f0a390d
Jonathan Goh <117988387+JonathanGoh95@users.noreply.github.com>
2025-04-29 10:09:03 +0800

    Week 1 Lecture Materials

---
+++ intro-to-javascript/app.js
@@ -0,0 +1,8 @@
+/*
+This code outputs the Hello World string to the Terminal
+*/
+const myName = "John";
+const friendName = "Jack";
+
+console.log(`My name is ${myName}.`);
+console.log(`My friend's name is ${friendName}.`)
