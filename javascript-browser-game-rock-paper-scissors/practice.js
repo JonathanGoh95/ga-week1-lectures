582fb17f806c8500c4a793ee765158115673694b
Jonathan Goh <jonathangohmh@gmail.com>
2025-05-09 10:36:52 +0800

    Added addtional comments

--- javascript-browser-game-rock-paper-scissors/practice.js
+++ javascript-browser-game-rock-paper-scissors/practice.js
@@ -1,10 +1,14 @@
 //Function to reverse string order and convert them to Uppercase
 const reverseUpperString = (str) => {
-  str = [...str] //Converts string to array
-    .reverse() //Reverses the array
-    .map((e) => e.toUpperCase()) //Converts each element in the array to Uppercase
-    .join(""); //Join the array back as a string
-  return str;
+  str = [...str]; //Converts string to array
+  // .reverse() //Reverses the array
+  let reverseString = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseString.push(str[i]);
+  }
+  // .map((e) => e.toUpperCase()) //Converts each element in the array to Uppercase
+  // .join(""); //Join the array back as a string
+  return reverseString.join("");
 };
 
 console.log(reverseUpperString("SEI Rocks!"));
