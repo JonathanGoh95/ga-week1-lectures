0f2c616312878035c4138bb3976dd16223163814
Jonathan Goh <jonathangohmh@gmail.com>
2025-05-20 20:56:12 +0800

    Tutorial Done

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+// Function names in JSX files must use Pascal Case, else fails
+const App = () => {
+// 'Return' can only return 'one' parent element, hence <></> (React Fragment) can be used to wrap multiple elements within.
+const todo = {text: 'A brand new task', done: true};
+const todos = [
+    {text: 'Learn JavaScript', done: true},
+    {text: 'Learn JSX', done: true},
+    {text: 'Learn HTML', done: true},
+    {text: 'Learn CSS', done: true},
+    {text: 'Master React', done: false},
+  ];
 
-function App() {
-  const [count, setCount] = useState(0)
+// Using the 'map' array iterator to loop through the array elements in React
+const todoList = todos.map((todo,index)=><li key={index}>{todo.done? `Task Completed: ${todo.text}` : `Task Not Yet Completed: ${todo.text}`}</li>);
 
-  return (
+return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="testClass">Javascript in JSX</h1>
+      {/* Only ternary expressions can be rendered within {}. If...else statements will NOT work.*/}
+      
+      <h2>Conditional Rendering</h2>
+      <p>{todo.done? `Task Completed: ${todo.text}`: todo.text}</p>
+      
+      <h2>Looping with JSX</h2>
+      <ul>
+        {todoList}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
