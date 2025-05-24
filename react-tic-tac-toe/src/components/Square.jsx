65a4bd3c415fdada38de7300fbc89d0d5e16cfd6
Jonathan Goh <jonathangohmh@gmail.com>
2025-05-25 00:46:48 +0800

    Functionality implemented

--- react-tic-tac-toe/src/components/Square.jsx
+++ react-tic-tac-toe/src/components/Square.jsx
@@ -1,3 +1,13 @@
-export default function Square({id}) {
-    return <button className="square">{id}</button>;
+import { useState } from "react";
+
+export default function Square({value, onSquareClick}) {
+    // const [value,setValue] = useState('')
+    
+    // const handleClick = () => {
+    //     setValue('X')
+    // }
+
+
+
+    return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
