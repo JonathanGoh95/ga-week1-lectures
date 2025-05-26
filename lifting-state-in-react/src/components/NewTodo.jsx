e90eecea50be40f6a8b0e8afba68fa2e233df98e
Jonathan Goh <117988387+JonathanGoh95@users.noreply.github.com>
2025-05-26 16:57:54 +0800

    Added relevant comments

--- lifting-state-in-react/src/components/NewTodo.jsx
+++ lifting-state-in-react/src/components/NewTodo.jsx
@@ -6,13 +6,16 @@ const NewTodo = ({addTodo}) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     // console.log("Creating a new to-do...");
+    // Adds a new todo upon clicking the submit button, after the newTodo state has been updated.
     addTodo(newTodo);
+    // Clears the input field after clicking the submit button
     setNewTodo('');
   };
 
 const [newTodo, setNewTodo] = useState('');
 
 const handleChange = (event) => {
+    // Updates the state as and when the user types in the input field
     setNewTodo(event.target.value);
 }
 
