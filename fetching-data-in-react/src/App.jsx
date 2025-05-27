098b7466b730bc7b7b373dda9c42fcae52974100
Jonathan Goh <117988387+JonathanGoh95@users.noreply.github.com>
2025-05-27 14:49:35 +0800

    Refactor

--- fetching-data-in-react/src/App.jsx
+++ fetching-data-in-react/src/App.jsx
@@ -3,19 +3,21 @@ import { useState } from 'react';
 import * as weatherService from './services/weatherService'
 
 const App = () => {
-  const [temp,setTemp] = useState('???')
-  const [feelsLike,setFeelsLike] = useState('???')
+  // const [temp,setTemp] = useState('???')
+  // const [feelsLike,setFeelsLike] = useState('???')
+  const [data,setData] = useState({})
 
   const handleClick = async () => {
-    const data = await weatherService.show('Singapore');
-    console.log('Data:', data);
-    setTemp(data.current.temp_c)
-    setFeelsLike(data.current.feelslike_c)
+    const dataset = await weatherService.show('Singapore');
+    console.log('Data:', dataset);
+    setData(dataset)
+  //   setTemp(data.current.temp_c)
+  //   setFeelsLike(data.current.feelslike_c)
   };
 
   return (
     <>
-    <h1>Weather Temperature in Singapore is {temp} degrees Celcius, but feels like {feelsLike} degrees Celcius.</h1>
+    <h1>Weather Temperature in Singapore is {data?.current?.temp_c} degrees Celcius, but feels like {data?.current?.feelslike_c} degrees Celcius.</h1>
     <button onClick={handleClick}>Check</button>
     </>
   );
